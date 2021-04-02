a22ddc50f736967c55cdb3cb2abd1eb1314782dd
Pedro Ramirez <prlara2000@gmail.com>
2021-04-02 13:31:41 -0500

    nav added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "contacts#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+   get '/about' => 'pages#about'
   resources :contacts
 end
